fd4742633c8277de7406e732df897270164474d0
Alexander Fisunov <fisunov.a.v@gmail.com>
2023-10-09 21:49:52 +0300

    hw3 ready

---
+++ src/main/java/ru/fisunov/homeworks/homework3/MainApp.java
@@ -0,0 +1,16 @@
+package ru.fisunov.homeworks.homework3;
+
+public class MainApp {
+    public static void main(String[] args) {
+        sum(1, 1);
+        sum2(2, 2);
+    }
+
+    public static void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void sum2(int a, int b) {
+        System.out.println(a + b + 1);
+    }
+}
