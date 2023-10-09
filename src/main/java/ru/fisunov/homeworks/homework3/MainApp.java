4bd85edc57486a0d87b020b89b75f4e0ba805204
Alexander Fisunov <fisunov.a.v@gmail.com>
2023-10-09 21:52:03 +0300

    hw3 fixed

--- src/main/java/ru/fisunov/homeworks/homework3/MainApp.java
+++ src/main/java/ru/fisunov/homeworks/homework3/MainApp.java
@@ -3,14 +3,14 @@ package ru.fisunov.homeworks.homework3;
 public class MainApp {
     public static void main(String[] args) {
         sum(1, 1);
-        sum2(2, 2);
+        sum2(2, 2, 2);
     }
 
     public static void sum(int a, int b) {
         System.out.println(a + b);
     }
 
-    public static void sum2(int a, int b) {
-        System.out.println(a + b + 1);
+    public static void sum2(int a, int b, int c) {
+        System.out.println(a + b + c);
     }
 }
